04d52311193e3bb67f3bb313669b2a12dd527cb3
Ibrahimsadiqbasha <ibrahimbasha0927@gmail.com>
2024-11-17 12:18:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add background color to the navbar on scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  
